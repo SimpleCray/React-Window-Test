e95be08c87ac686d4bd0f95f8ab2181ea7dca187
haiduong <ngochaiitech@gmail.com>
2024-12-08 15:51:33 +0700

    restructure folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import styled from "@emotion/styled";
-import Container from "./Container";
+import Container from "./components/Container";
 
 const MainDiv = styled.div`
   display: flex;
@@ -10,9 +10,6 @@ const MainDiv = styled.div`
 export default function App() {
   return (
     <MainDiv className="App">
-      {/* <div style={{ width: "300px" }}>
-        <nobr>Flex demo</nobr>
-      </div> */}
       <Container />
     </MainDiv>
   );
